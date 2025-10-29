4047fca8313c88ec07ef98ef653c8e09c7634f2c
Emory Merryman <emory.merryman@gmail.com>
2025-10-28 22:01:13 -0400

    dot-ssh

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
                                                                             let
                                                                                 mapper =
                                                                                     name : value :
-                                                                                    "# name" ;
+                                                                                    "# name ${ name }" ;
                                                                                 in builtins.mapAttrs mapper configuration ;
                                                                         bash-name = host-name : attribute-name : builtins.concatStringsSep "" [ "V" ( builtins.hashString "sha512" ( builtins.concatStringsSep "c80c2687f8aa97ca4b3b44626d06d366a93fa8c67de5cf52d565b17b48334603aad79b5cb3d293f54f6084df628e343f71f4704bff525c840e8435e9fa1cad27" [ host-name attribute-name ] ) ) ] ;
                                                                         dot-ssh =
