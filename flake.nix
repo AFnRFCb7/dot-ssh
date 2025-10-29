e3ca27df899ef164761ca93bf19e0a623c944e6d
Emory Merryman <emory.merryman@gmail.com>
2025-10-28 22:30:53 -0400

    dot-ssh

--- flake.nix
+++ flake.nix
@@ -38,7 +38,8 @@
                                                                                                         string = path : value : [ ( builtins.concatStringsSep "" [ ( bash-name host-name ( builtins.elemAt path 0 ) ) ] ) ]  ;
                                                                                                     }
                                                                                                     value ;
-                                                                                            in builtins.concatStringsSep "\n" ( builtins.attrValues v ) ;
+                                                                                            # in builtins.concatStringsSep "\n" ( builtins.attrValues v ) ;
+                                                                                            in "# host-name = ${ host-name }
                                                                                 in builtins.mapAttrs mapper configuration ;
                                                                         bash-name = host-name : attribute-name : builtins.concatStringsSep "" [ "V" ( builtins.hashString "sha512" ( builtins.concatStringsSep "c80c2687f8aa97ca4b3b44626d06d366a93fa8c67de5cf52d565b17b48334603aad79b5cb3d293f54f6084df628e343f71f4704bff525c840e8435e9fa1cad27" [ host-name attribute-name ] ) ) ] ;
                                                                         dot-ssh =
