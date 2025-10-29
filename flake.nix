7a8542648f96016bf7913874a2b349abe4583e5d
Emory Merryman <emory.merryman@gmail.com>
2025-10-29 07:55:35 -0400

    dot-ssh

--- flake.nix
+++ flake.nix
@@ -155,7 +155,7 @@
                                                                                                     {
                                                                                                         bool = path : value : [ ] ;
                                                                                                         int = path : value : [ ] ;
-                                                                                                        lambda = path : value : [ ( builtins.concatStringsSep " " [ "ln" "--symbolic" ( bash-name host-name ( builtins.elemAt path 0 ) ) "/links" ] ) ] ;
+                                                                                                        lambda = path : value : [ ( builtins.concatStringsSep " " [ "ln" "--symbolic" ( builtins.concatStringsSep "" [ "$" "{" ( bash-name host-name ( builtins.elemAt path 0 ) ) "}" ] ) "/links" ] ) ] ;
                                                                                                         string = path : value : [ ] ;
                                                                                                     }
                                                                                                     value ;
