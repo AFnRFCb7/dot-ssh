cb6a4bbefd2ed9a53804bc51b5ce0e91639e23b4
Emory Merryman <emory.merryman@gmail.com>
2025-10-30 07:19:05 -0400

    dot-ssh

--- flake.nix
+++ flake.nix
@@ -107,17 +107,6 @@
                                                                                             user-known-hosts-file ? null
                                                                                         } @value :
                                                                                             let
-                                                                                                mapper =
-                                                                                                    attribute-name : attribute-value :
-                                                                                                        let
-                                                                                                            left-name = bash-name host-name attribute-name ;
-                                                                                                            right-name = builtins.replaceStrings [ "-a" "-b" "-c" "-d" "-e" "-f" "-g" "-h" "-i" "-j" "-k" "-l" "-m" "-n" "-o" "-p" "-q" "-r" "-s" "-t" "-u" "-v" "-w" "-x" "-y" "-z" ] [ "A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z" ] ( builtins.concatStringsSep "" [ "-" attribute-name ] ) ;
-                                                                                                            in
-                                                                                                                if builtins.typeOf attribute-value == "lambda" then
-                                                                                                                    let
-                                                                                                                        x = attribute-value { resources = resources ; self = self ; } ;
-                                                                                                                        in [ "${ left-name } ${ right-name }/${ x.file }" ]
-                                                                                                                else [ "${ left-name } ${ right-name }" ] ;
                                                                                                 in
                                                                                                     builtins.concatStringsSep "\n" ( builtins.concatLists [ [ "HostName ${ host-name }" ] ] ) ;
                                                                             in builtins.mapAttrs mapper configuration ;
