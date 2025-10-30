20022429bbf53f46e22fac101f3d2b2c88044bb1
Emory Merryman <emory.merryman@gmail.com>
2025-10-30 07:51:08 -0400

    dot-ssh

--- flake.nix
+++ flake.nix
@@ -110,7 +110,7 @@
                                                                                                     attribute-name : attribute-value :
                                                                                                         let
                                                                                                             left-name = bash-name host-name attribute-name ;
-                                                                                                            right-name = builtins.replaceStrings [ "-a" "-b" "-c" "-d" "-e" "-f" "-g" "-h" "-i" "-j" "-k" "-l" "-m" "-n" "-o" "-p" "-q" "-r" "-s" "-t" "-u" "-v" "-w" "-x" "-y" "-z" ] [ "A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z" ] ( builtins.concatStringsSep "" [ "-" attribute-name ] )
+                                                                                                            right-name = builtins.replaceStrings [ "-a" "-b" "-c" "-d" "-e" "-f" "-g" "-h" "-i" "-j" "-k" "-l" "-m" "-n" "-o" "-p" "-q" "-r" "-s" "-t" "-u" "-v" "-w" "-x" "-y" "-z" ] [ "A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z" ] ( builtins.concatStringsSep "" [ "-" attribute-name ] ) ;
                                                                                                             in
                                                                                                                 if builtins.typeOf attribute-value == "lambda" then
                                                                                                                     let
