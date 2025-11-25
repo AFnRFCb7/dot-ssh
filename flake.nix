c5d0110ee88cf33cbc7a134d52e6f0b469165e3e
Emory Merryman <emory.merryman@gmail.com>
2025-11-25 13:04:40 -0500

    up

--- flake.nix
+++ flake.nix
@@ -109,7 +109,7 @@
                                                                                                             in
                                                                                                                 if builtins.typeOf attribute-value == "lambda" then
                                                                                                                     let
-                                                                                                                        x = attribute-value { mount = mount ; pkgs = pkgs ; resources = resources ; stage = stage ; } ;
+                                                                                                                        x = attribute-value { mount = mount ; pkgs = pkgs ; resources = resources ; } ;
                                                                                                                         in "${ right-name } ${ builtins.concatStringsSep "" [ "$" "{" left-name "}" ] }/${ x.file }"
                                                                                                                 else "${ right-name } ${ builtins.concatStringsSep "" [ "$" "{" left-name "}" ] }" ;
                                                                                                 in
