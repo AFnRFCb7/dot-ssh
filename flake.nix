d8b476a266fdfa246ee99c67cfdb0d996003581d
Emory Merryman <emory.merryman@gmail.com>
2025-11-26 22:07:22 -0500

    fun

--- flake.nix
+++ flake.nix
@@ -38,8 +38,8 @@
                                                                                                                     # ''ln --symbolic ${ resource-name } /mount/stage/${ resource-name }''
                                                                                                                 ]
                                                                                                     else builtins.throw "ssh resources is wrongly nested.  values must be two levels deep, but ${ builtins.toJSON path } is ${ builtins.toString ( builtins.length path ) } levels deep." ;
-                                                                                            list = [ ] ; # concat.list ;
-                                                                                            set = [ ] ; # concat.set ;
+                                                                                            list = concat.list ;
+                                                                                            set = concat.set ;
                                                                                         }
                                                                                         implementation-resources ;
                                                                                 beta =
