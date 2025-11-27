72ce0597516b391d70907770d81c6247121c4408
Emory Merryman <emory.merryman@gmail.com>
2025-11-27 09:42:08 -0500

    wtf

--- flake.nix
+++ flake.nix
@@ -65,7 +65,6 @@
                                                                                                 else
                                                                                                     let
                                                                                                         value-name = builtins.concatStringsSep "" [ "B" ( builtins.hashString "sha512" ( builtins.toJSON path ) ) ] ;
-                                                                                                        in [ ''${ value-name }=${ value }'' ''export ${ value-name }'' ] ;
                                                                                         in
                                                                                             visitor
                                                                                                 {
