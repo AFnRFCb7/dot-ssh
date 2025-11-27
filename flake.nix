9e6d5a028c91bfe18d32edb7ed2a5374d08f88aa
Emory Merryman <emory.merryman@gmail.com>
2025-11-27 10:29:35 -0500

    wtf

--- flake.nix
+++ flake.nix
@@ -154,7 +154,7 @@
                                                                                                         mapper =
                                                                                                             name : value :
                                                                                                                 let
-                                                                                                                    configuration-name = builtins.replaceStrings [ "-a" "-b" "-c" "-d" "-e" "-f" "-g" "-h" "-i" "-j" "-k" "-l" "-m" "-n" "-o" "-p" "-q" "-r" "-s" "-t" "-u" "-v" "-w" "-x" "-y" "-z" ] [ "A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z" ] "_${ name }" ;
+                                                                                                                    configuration-name = builtins.replaceStrings [ "-a" "-b" "-c" "-d" "-e" "-f" "-g" "-h" "-i" "-j" "-k" "-l" "-m" "-n" "-o" "-p" "-q" "-r" "-s" "-t" "-u" "-v" "-w" "-x" "-y" "-z" ] [ "A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z" ] "-${ name }" ;
                                                                                                                     path = [ host-name name ] ;
                                                                                                                     string =
                                                                                                                         ''
